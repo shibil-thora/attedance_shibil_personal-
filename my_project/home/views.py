5c6803e02529c69f69c8ff59a6f52e345759d3a8
shibil-thora <shibilallakkat123@gmail.com>
2024-08-31 10:32:07 +0530

    changed url

--- my_project/home/views.py
+++ my_project/home/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from rest_framework.views import APIView  
-from rest_framework.response import Response
+from rest_framework.response import Response 
+from firebase_admin import credentials, db 
+import firebase_admin 
+import datetime
+
+cred = credentials.Certificate('serviceAccountKey.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://marketing-data-d141d-default-rtdb.firebaseio.com'
+})
+
 
 class GetEmployees(APIView): 
-    def get(self, request): 
+    def get(self, request):  
+        data = {
+            'name': 'Onwords', 
+            'place': 'chinniyampalayam', 
+            'state': 'Tamilnadu', 
+            'time': datetime.datetime.now()
+        }
         return Response('hi from shibil')
